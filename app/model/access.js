29464a8acac552f5ac8801e932a139dd7956df59
xiaoqiaoWEB <346043591@qq.com>
2019-09-30 22:43:46 +0800

    j

--- app/model/access.js
+++ app/model/access.js
@@ -1,3 +1,5 @@
+'use strict';
+
 module.exports = app => {
   const mongoose = app.mongoose;
   const Schema = mongoose.Schema;
@@ -6,38 +8,38 @@ module.exports = app => {
 
   const AccessSchema = new Schema({
     module_name: {
-      type: String
+      type: String,
     },
     action_name: {
-      type: String
+      type: String,
     },
-    //节点类型 :  1、表示模块   2、表示菜单   3、操作
+    // 节点类型 :  1、表示模块   2、表示菜单   3、操作
     type: {
-      type: Number
+      type: Number,
     },
-    url:{
-      type:String
+    url: {
+      type: String,
     },
-    //此module_id和当前模型的_id关联     module_id= 0 表示模块
+    // 此module_id和当前模型的_id关联     module_id = 0 表示模块
     module_id: {
       // 混合类型
-      type: Schema.Types.Mixed
+      type: Schema.Types.Mixed,
     },
-    sort:{
-      type:Number,
-      default:100
+    sort: {
+      type: Number,
+      default: 100,
     },
     desc: {
-      type:String
+      type: String,
     },
-    status:{
-      type:Number,
-      default:1
+    status: {
+      type: Number,
+      default: 1,
     },
-    add_time:{
-      type:Number,
-      default:d.getTime()
-    }
+    add_time: {
+      type: Number,
+      default: d.getTime(),
+    },
   });
   return mongoose.model('Access', AccessSchema, 'access');
 }
