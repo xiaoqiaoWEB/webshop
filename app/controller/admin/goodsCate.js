111e6d736b5cb5b407be8e31eff13f50abb8ab24
xiaoqiaoWEB <346043591@qq.com>
2019-11-02 19:09:32 +0800

    doogcate add.inhtml

--- app/controller/admin/goodsCate.js
+++ app/controller/admin/goodsCate.js
@@ -1,4 +1,6 @@
 'use strict';
+const fs = require('fs');
+const pump = require('mz-modules/pump');
 
 const BaseController = require('./base.js');
 
@@ -8,11 +10,37 @@ class GoodsCateController extends BaseController {
   }
 
   async add() {
-    await this.ctx.render('admin/goodsCate/add');
+    let result = this.ctx.model.GoodsCate.find({'pid': 0});
+    await this.ctx.render('admin/goodsCate/add', {
+      cateList: result,
+    });
   }
 
   async doAdd() {
-    
+    // autoFields: true  获得上传的 除去文件之外的信息
+    // 数据流  parts
+    let parts = this.ctx.multipart({autoFields: true});
+    let files = {};
+    let stream;
+    while ( (stream = await parts()) != null) {
+      if ( !stream.filename ) {
+        break;
+      }
+      let fieldname = stream.fieldname;
+      // 目录
+      let dir = await this.service.tools.getUploadFile(stream.filename);
+      let target = dir.uploadDir;
+      let writeStream = fs.createWriteStream(target);
+
+      await pump(stream, writeStream); 
+
+      files = Object.assign(files, {
+        [fieldname]: dir.saveDir,
+      });
+    }
+    let saveData = new this.ctx.model.GoodsCate(Object.assign(files, parts.field));
+    await saveData.save();
+    await this.success('/admin/goodsCate', '增加商品分类成功！');
   }
 
   async edit() {
