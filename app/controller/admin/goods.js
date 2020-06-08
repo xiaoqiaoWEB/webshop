f1f0df433d7e116ab2cb0434079436375d3a0b20
xiaoqiaoWEB <346043591@qq.com>
2020-06-08 19:42:17 +0800

    goods add 01
    Signed-off-by: xiaoqiaoWEB <346043591@qq.com>

--- app/controller/admin/goods.js
+++ app/controller/admin/goods.js
@@ -14,7 +14,33 @@ class GoodsController extends BaseController {
   }
 
   async add() {
-    this.ctx.body = 'add';
+    let { ctx } = this;
+    // 所有的颜色
+    let colorList = await ctx.model.GoodsColor.find({});
+    // 商品类型
+    let goodsType = await ctx.model.GoodsType.find({});
+    // 商品分类
+    let goodsCate = await ctx.model.GoodsCate.aggregate([
+      {
+        $lookup: {
+          from: 'goods_cate',
+          localField: '_id',
+          foreignField: 'pid',
+          as: 'items',
+        },
+      },
+      {
+        $match: {
+          pid: '0',
+        },
+      },
+    ]);
+
+    await this.ctx.render('admin/goods/add', {
+      colorResult: colorList,
+      goodsType,
+      goodsCate,
+    });
   }
 
   async doAdd() {
